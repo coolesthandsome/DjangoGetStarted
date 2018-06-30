bbb0c05e218ab91cabf2563ca623c1f772088335
coolesthandsome <195241163@qq.com>
2018-06-30 21:07:30 +0800

    Review_LeaveMessages

--- DjangoGetStarted/urls.py
+++ DjangoGetStarted/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from message import views
+from apps.message import views
+
+from apps.message.views import getform
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('form/',getform)
 ]
